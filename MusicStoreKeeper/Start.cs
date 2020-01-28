82b12608face0f5bfa9588963ebb07d0de88f3ac
DrProkop <prokopovich.dmitry@gmail.com>
2020-01-29 01:33:56 +0300

    Initial commit

---
+++ MusicStoreKeeper/Start.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+
+namespace MusicStoreKeeper
+{
+    public class Start
+    {
+        public static void Configure()
+        {
+
+        }
+
+        public static void Run()
+        {
+
+        }
+    }
+}
