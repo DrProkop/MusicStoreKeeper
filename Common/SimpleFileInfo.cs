688c83299b0846a50997bb7b033b602fde8fa813
DrProkop <prokopovich.dmitry@gmail.com>
2020-02-04 15:06:29 +0300

    Add icons for tree view items

--- Common/SimpleFileInfo.cs
+++ Common/SimpleFileInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -12,30 +13,50 @@ namespace Common
     //TODO: Implement INotifyPropertyChanged
     public interface ISimpleFileInfo
     {
+        string Icon { get; }
         FileInfo Info { get; }
         string Name { get; }
+        string Extension { get; }
         bool IsDirectory { get; }
+        bool IsImage { get; }
+        bool IsTextDocument { get; }
+        bool IsAudioFile { get; }
         ObservableCollection<ISimpleFileInfo> Children { get; set; }
     }
 
     public class SimpleFileInfo : ISimpleFileInfo
     {
+        //TODO: Move file type definition to separate class
+        private static readonly string[] ImageExtensions = {".JPG", ".BMP", ".PNG", ".GIF"};
+        private static readonly string[] TextExtensions = { ".TXT", ".LOG", ".MD"};
+        private static readonly string[] AudioExtensions = { ".MP3", ".FLAC" };
         private readonly FileInfo _fInfo;
 
 
         public SimpleFileInfo(FileInfo fInfo)
         {
             _fInfo = fInfo;
-            
         }
 
         #region [  properties  ]
 
+        
         public FileInfo Info => _fInfo;
 
         public string Name => _fInfo.Name;
 
+        public string Extension => _fInfo.Extension.ToUpper();
+
         public bool IsDirectory => _fInfo.Attributes == FileAttributes.Directory;
+
+        public bool IsImage=>ImageExtensions.Contains(Extension);
+        
+        public bool IsTextDocument=>TextExtensions.Contains(Extension);
+
+        public bool IsAudioFile => AudioExtensions.Contains(Extension);
+    
+
+        public string Icon { get; }
 
         private ObservableCollection<ISimpleFileInfo> _children = new ObservableCollection<ISimpleFileInfo>();
 
@@ -44,6 +65,8 @@ namespace Common
             get => _children;
             set { _children = value; }
         }
+
+
 
         #endregion
 
@@ -54,7 +77,12 @@ namespace Common
     {
         public FileInfo Info { get; }
         public string Name => string.Empty;
+        public string Extension => string.Empty;
         public bool IsDirectory => false;
+        public bool IsImage => false;
+        public bool IsTextDocument => false;
+        public bool IsAudioFile => false;
+        public string Icon { get; }
         public ObservableCollection<ISimpleFileInfo> Children { get; set; }
     }
 }
---
+++ Vmv/View/Converters/TreeViewIconConverter.cs
@@ -0,0 +1,69 @@
+﻿using Common;
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Vmv.View.Converters
+{
+    /// <summary>
+    /// Returns icon image sources for different file types.
+    /// </summary>
+    public class TreeViewIconConverter : IValueConverter
+    {
+        /// <summary>Converts a value. </summary>
+        /// <param name="value">The value produced by the binding source.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>A converted value. If the method returns null, the valid null value is used.</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is ISimpleFileInfo item)
+            {
+                var converter = new ImageSourceConverter();
+                var resName = "pack://application:,,,/ResourceLibrary;component/Images/TreeViewIcons/close_folder_icon.png";
+
+                if (item is DummySimpleFileInfo)
+                {
+                    return DependencyProperty.UnsetValue;
+                }
+                if (item.IsDirectory)
+                {
+                    return "pack://application:,,,/ResourceLibrary;component/Images/TreeViewIcons/close_folder_icon.png";
+
+                    //var source = converter.ConvertFromString(resName) as ImageSource;
+                    //return source;
+                }
+                if (item.IsAudioFile)
+                {
+                    return "pack://application:,,,/ResourceLibrary;component/Images/TreeViewIcons/audio_file_icon.png";
+                }
+                if (item.IsImage)
+                {
+                    return "pack://application:,,,/ResourceLibrary;component/Images/TreeViewIcons/image_file_icon.png";
+                }
+                if (item.IsTextDocument)
+                {
+                    return "pack://application:,,,/ResourceLibrary;component/Images/TreeViewIcons/document_icon.png";
+                }
+
+                return "pack://application:,,,/ResourceLibrary;component/Images/TreeViewIcons/document_blank_icon.png";
+            }
+
+            return DependencyProperty.UnsetValue;
+        }
+
+        /// <summary>Converts a value. </summary>
+        /// <param name="value">The value that is produced by the binding target.</param>
+        /// <param name="targetType">The type to convert to.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>A converted value. If the method returns null, the valid null value is used.</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
